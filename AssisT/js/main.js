62cea4062ae67471b3ff56d5bb0b90bbed1acb52
morin0094 <47797871+morin0094@users.noreply.github.com>
2021-01-25 13:28:34 -0500

    Add files via upload

--- AssisT/js/main.js
+++ AssisT/js/main.js
@@ -1,53 +1,169 @@
-﻿//Ouvrir un popup en mobile
-$(".openPopin").click(function () {
-  if ($(window).width() < 768) {
-    $(window).scrollTop(0);
+﻿(function () {
+  var slider = tns({
+    container: '.homeSlider',
+    items: 4,
+    gutter: 24,
+    loop: false,
+    speed: 700,
+    slideBy: 'page',
+    controlsText: ["", ""],
+    responsive: {
+      1735: {
+        items: 4
+      },
+      768: {
+        items: 3
+      },
+      320: {
+        items: 1,
+        gutter: 12
+      },
+    }
+  });
+
+  var slider2 = tns({
+    container: '.homeImages',
+    loop: false,
+    mode: 'gallery',
+    speed: 700
+  });
+
+  document.querySelectorAll('.homeSlider a').forEach((slide, i) => {
+    slide.addEventListener("mouseover", function () {
+      slider2.goTo(i);
+    });
+  });
+
+  document.querySelectorAll('#bigMenu .category, #bigMenu .subCategory').forEach((category, i) => {
+    category.addEventListener("click", function () {
+      category.classList.contains("open") ? category.classList.remove("open") : category.classList.add("open");
+    });
+  });
+
+  function updateProgressbar() {
+    var sliderInfo = slider.getInfo();
+    var barWidth = Math.round(100 / sliderInfo.slideCount) * (sliderInfo.items * (sliderInfo.navCurrentIndex + 1));
+    if (barWidth > 100) {
+      barWidth = 100;
+    }
+    document.querySelector(".progress .bar").setAttribute("style", "width:" + barWidth + "%")
   }
-});
 
-setImgInBg();
-$(".defaultFacet").bind("templatingCompleted", function () {
-	setImgInBg();
-});
+  slider.events.on("transitionStart", updateProgressbar);
 
-//Ajout de classe pour notre beau IE
-if (navigator.userAgent.indexOf("Trident") > -1) {
-  document.getElementsByTagName("html")[0].classList.add("IE11");
+  updateProgressbar();
+
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      document.getElementById("loading").classList.add("loaded");
+      document.getElementById("app").classList.add("loaded");
+      document.getElementById("home").classList.add("open");
+      document.getElementById("home").classList.add("loaded");
+      document.querySelectorAll(".easein").forEach(visual => {
+        visual.classList.add("loaded");
+      });
+    }
+  };
+})();
+
+function openBigMenu() {
+  document.getElementById("bigMenu").classList.add("open");
 }
 
-if (navigator.userAgent.indexOf("Edge") > -1) {
-  document.getElementsByTagName("html")[0].classList.add("IEedge");
+function closeBigMenu() {
+  document.getElementById("bigMenu").classList.remove("open");
 }
 
-//Faire afficher le menu mobile
-$(".toggleMenuMobile").click(function(){
-  $(this).toggleClass("selected");
-  $(".menuMobile").slideToggle();
-});
+function openContactPanel() {
+  document.getElementById("contactPanel").classList.add("open");
+}
 
-//Fonctionnement en toggle du menu principal mobile
-$(".menuMobile").find("li.parent").each(function () {
-  //Enleve un paquet d'evenements qu'on a plus besoin, enleve le href sur le lien, et change quelques css
-  $(this).removeAttr("onkeydown").removeAttr("onkeypress").removeAttr("expansionmode").find(">a").removeAttr("href").next().hide();
-  $(this).find("*").css("visibility", "visible");
-  $(this).parents("ul.MenuBar").removeClass("MenuBar");
+function closeContactPanel() {
+  document.getElementById("contactPanel").classList.remove("open");
+}
 
-  //A chaque click sur sur les liens d'un li parent, ferme les sous-menus puis ouvre celui qu'on veut
-  $(this).find(">a").click(function () {
-    if ($(this).next().css("display") === "block") {
-      $(this).closest("ul").find("div").slideUp(500, function () {
-				$(window).scrollTop(0);
-      });
-      $(this).parent().removeClass("open");
-    }
-    else {
-      var currentElement = $(this);
-      currentElement.closest("ul").find("li").removeClass("open").find("div").slideUp();
-			currentElement.next().slideDown(500, function () {
-				$(window).scrollTop(currentElement.offset().top);
-      });
-      currentElement.parents("ul").find("a").removeClass("open");
-      currentElement.parent().addClass("open");
-    }
-  })
-});
+function openBulletinPanel() {
+  document.getElementById("bulletinPanel").classList.add("open");
+}
+
+function closeBulletinPanel() {
+  document.getElementById("bulletinPanel").classList.remove("open");
+}
+
+function showDetails(section) {
+  document.getElementById("home").classList.remove("open");
+  document.getElementById("home").classList.remove("loaded");
+  document.getElementById("details").classList.add("open");
+  let selectedMenu = document.querySelector(".leftMenu .selected");
+  let roundedMenu = document.querySelector(".leftMenu .rounded");
+  if (selectedMenu) {
+    selectedMenu.classList.remove("selected");
+  }
+  if (roundedMenu) {
+    roundedMenu.classList.remove("rounded");
+  }
+  let selectedSection = document.querySelector(".wrapSection.open");
+  if (selectedSection) {
+    selectedSection.classList.remove("open");
+  }
+  let imgSection = document.querySelector(".detailsImgs .open");
+  if (imgSection) {
+    imgSection.classList.remove("open");
+  }
+  document.getElementById("menu_" + section).classList.add("selected");
+  document.getElementById("img_" + section).classList.add("open");
+  if (document.getElementById("menu_" + section).previousElementSibling) {
+    document.getElementById("menu_" + section).previousElementSibling.classList.add("rounded");
+  }
+  document.getElementById(section).classList.add("open");
+}
+
+function mainScreen() {
+  document.getElementById("home").classList.add("open");
+  document.getElementById("details").classList.remove("open");
+}
+
+function switchDetails(section) {
+  let selectedMenu = document.querySelector(".leftMenu .selected");
+  let roundedMenu = document.querySelector(".leftMenu .rounded");
+  if (selectedMenu) {
+    selectedMenu.classList.remove("selected");
+  }
+  if (roundedMenu) {
+    roundedMenu.classList.remove("rounded");
+  }
+  let selectedSection = document.querySelector(".wrapSection.open");
+  let fadeOutSection = document.querySelector(".wrapSection.fadeOut");
+  if (selectedSection) {
+    selectedSection.classList.remove("open");
+    selectedSection.classList.add("fadeOut");
+    setTimeout(function () {
+      selectedSection.classList.remove("fadeOut");
+    }, 1200);
+  }
+  document.getElementById("menu_" + section).classList.add("selected");
+  if (document.getElementById("menu_" + section).previousElementSibling) {
+    document.getElementById("menu_" + section).previousElementSibling.classList.add("rounded");
+  }
+  let imgSection = document.querySelector(".detailsImgs .open");
+  if (imgSection) {
+    imgSection.classList.remove("open");
+  }
+  document.getElementById("img_" + section).classList.add("open");
+  document.getElementById(section).classList.add("fadeIn");
+  setTimeout(function () {
+    document.getElementById(section).classList.add("open");
+  }, 10);
+  setTimeout(function () {
+    document.getElementById(section).classList.remove("fadeIn");
+  }, 1200);
+}
+
+function menuNavigation(section) {
+  closeBigMenu()
+  if (document.getElementById("home").classList.contains("open")) {
+    showDetails(section);
+  } else {
+    switchDetails(section);
+  }
+}
